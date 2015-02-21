f2d2e513ac23fb83c5f5f97c1f9b1523c64a30d3
Gary Thung <garythung@gmail.com>
2015-02-21 01:36:52 -0800

    Edited templating to vanilla HTML

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var path = require('path');
 var port = process.env.PORT || 8000;
 var mongoose = require('mongoose');
 var configDB = require('./config/database.js');
+var twitter = require('twitter')
 
 // =====================================
 // DATABASE ============================
@@ -18,16 +19,28 @@ mongoose.connect(configDB.url); // connect to our database
 // VIEW ENGINE =========================
 // =====================================
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
+// =====================================
+// CONFIGURATION =======================
+// =====================================
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser()); // get information from html forms
 app.use(express.static(path.join(__dirname, 'public')));
 
 // =====================================
+// TWITTER =============================
+// =====================================
+var client = new twitter({
+	consumer_key: 'qbc7jJLc6pHCJl8tUKFdg2SBF',
+	consumer_secret: 'X6KQRNHHPk2JUpSsMCTgKtDPfALoSzBmD4AV9EBlvP6D8DPNzv',
+	access_token_key: '486385775-7aRBhZ0BUMj9I3yKbZdMpvqKPwPriB1M99WnlMdj',
+	access_token_secret: '7qOrD7dzLzJnnLJBgNBouUMqMoOth6rDdCcv22OMGQImv'
+});
+
+// =====================================
 // ROUTES ==============================
 // =====================================
-require('./routes.js')(app); // load our routes and pass in our app and fully configured passport
+require('./routes.js')(app, client); // load our routes and pass in our app and fully configured passport
 
 // =====================================
 // LAUNCH ==============================
--- routes.js
+++ routes.js
@@ -1,12 +1,21 @@
 // routes.js
 
-module.exports = function(app) {
+module.exports = function(app, client) {
     // =====================================
     // HOME PAGE ===========================
     // =====================================
     app.get('/', function(req, res) {
-        res.render('index.jade', {
-            title: 'Heatmap'
+        console.log(__dirname + '/views/index.html');
+        res.sendFile(__dirname + '/views/index.html');
+        client.stream('statuses/filter', {track: 'isis'},  function(stream) {
+            // stream.on('data', function(tweet) {
+            //     var tweets = document.getElementById("tweets");
+            //     tweets.innerHTML += "<p>" + tweet.text + "</p>"
+            // });
+
+            // stream.on('error', function(error) {
+            //     console.log(error);
+            // });
         });
     });
 };
